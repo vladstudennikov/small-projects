f19c43e9afd4d13888f7b501f452fc74dff61014
vladstudennikov <91913216+vladstudennikov@users.noreply.github.com>
2022-10-31 11:35:09 +0200

    Add files via upload

---
+++ oop/lab 4 oop/lab 4 oop/QueueIsClearedEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_4_oop
+{
+    public class QueueIsClearedEventArgs : EventArgs
+    {
+        public bool isCleared { get; set; }
+    }
+}
